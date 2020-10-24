c2c7504eee77ade6277af40b5136b052e837fbf5
MefitHp <mefitdev@gmail.com>
2020-10-23 21:13:39 -0500

    lol?

--- components/sections/index.js
+++ components/sections/index.js
@@ -1,6 +1,6 @@
 import { AboutMe } from './aboutMe';
 import { Intro } from './intro';
 import { Location } from './location';
-import { WhatDoWeDo } from './whatDoWeDo';
+import { WhatDoWeDo } from './WhatDoWeDo';
 
 export { AboutMe, Intro, Location, WhatDoWeDo };
